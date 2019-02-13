97e0523cc15606f4157cbda859f31667a71985cd
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-13 19:20:05 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -5,10 +5,9 @@ end
 
 def word_substituter(tweet)
     #convert the tweet into an array, so that it can be iterated over:
-    tweet.split(" ")
-    binding.pry
+    split_tweet = tweet.split(" ")
     #now that the tweet is an array, iterate over it to compare each word (element) to each original word (key) in the dictionary!
-    tweet.each {|words| words.include? dictionary.keys}
+    split_tweet.each {|words| words.include? dictionary.keys}
 binding.pry
     #Make sure to convert the shortened tweet back to a string!
 end
