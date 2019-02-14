3da98ba33575765aca397ba6df9b8deb6b14e8fe
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-14 16:44:32 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,17 +1,19 @@
 require 'pry'
 def dictionary
 #  {"hello"=>"hi", ("to"||"two"||"too") => "2",("four"||"for") => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
-  {"hello"=>"hi", ["to", "two", "too"] => "2",["four", "for"] => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
+  {"hello"=>"hi", "to" => "2", "two" => "2", "too" => "2", "four" => "4", "for" => "4" , "be" => "b", "you" => "u", "at" => "@", "and" => "&" }
 
 end
 
 def word_substituter(tweet)
     #convert the tweet into an array, so that it can be iterated over:
     split_tweet = tweet.split(" ")
-# binding.pry
     #now that the tweet is an array, iterate over it to compare the dictionary's original words (keys) to if they match any of the words in the tweets!
-    split_tweet.each {|words| dictionary.keys.include? words}
+    split_tweet.each {|words| (dictionary.keys).include?(words)}
 binding.pry
+      # if true
+      #   #replace long word(key) with shortened word(value)
+      # else
+      #   return "This tweet is as short as it will get!"
     #Make sure to convert the shortened tweet back to a string!
-
 end
