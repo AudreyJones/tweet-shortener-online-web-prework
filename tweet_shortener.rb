fc55590310ae655ed5a28440aa5fa6d58192d0df
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-14 16:38:20 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -8,13 +8,13 @@ end
 def word_substituter(tweet)
     #convert the tweet into an array, so that it can be iterated over:
     split_tweet = tweet.split(" ")
-# binding.pry
+binding.pry
     #now that the tweet is an array, iterate over it to compare the dictionary's original words (keys) to if they match any of the words in the tweets!
     split_tweet.each {|words| dictionary.keys.include?(words)
       if true
 
     }
-binding.pry
+#binding.pry
     #Make sure to convert the shortened tweet back to a string!
 
 end
