84cf9e7aa71b453ce3efa0f89afcc5cfdb66cfee
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-14 16:42:39 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -9,13 +9,13 @@ def word_substituter(tweet)
     #convert the tweet into an array, so that it can be iterated over:
     split_tweet = tweet.split(" ")
     #now that the tweet is an array, iterate over it to compare the dictionary's original words (keys) to if they match any of the words in the tweets!
-    split_tweet.each {|words| (dictionary.keys).include?(words)
+    split_tweet.each {|words| (dictionary.keys).include?(words)}
 binding.pry
       if true
         #replace long word(key) with shortened word(value)
       else
         return "This tweet is as short as it will get!"
-    }
+    
 
     #Make sure to convert the shortened tweet back to a string!
 
